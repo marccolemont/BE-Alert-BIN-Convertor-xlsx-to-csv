1dfdc980e071dda4a91af8ec977e0a8e1c60261c
Marc Colemont <marc.colemont@mc-productions.be>
2025-12-20 18:52:08 +0100

    Release1

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
+
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/app.ico"); // path to your .ico
+        res.compile().unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 // src/main.rs
 // BE-Alert xlsx to cvs convertor 
 // -------------------------------
